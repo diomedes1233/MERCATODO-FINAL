89a721fedce24988956ebeec6222a7b4457d708c
diomedes1233 <83196146+diomedes1233@users.noreply.github.com>
2021-10-12 09:55:01 -0500

    primera verción

---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h2>Merca</h2>
+      <h1>TODO</h1>
+      <h2>
+        TE OFRECEMOS LA MEJOR ATENCIÓN, CON LOS MEJORES PRODUCTOS PARA TI Y TODA
+        TU FAMILIA, VEN PARA TENER EL GUSTO DE ATENDERTE
+      </h2>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/layout/header/Header'
+import { PagMain } from '../components/pagMain/PagMain'
+import Footer from '../components/layout/footer/Footer'
+
+function Main() {
+  return (
+    <div>
+      <Header />
+      <PagMain />
+      <Footer />
+    </div>
+  )
+}
+
+export { Main }
